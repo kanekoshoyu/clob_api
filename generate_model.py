a98cf9699325b9dc2b6c6acc0ec4ba515e16bb18
Sho Kaneko <kanekoshoyu@gmail.com>
2024-07-31 13:42:38 +0800

    start rewrite codegen in rust

--- generate_model.py
+++ generate_model.py
@@ -15,7 +15,6 @@ class Language(Enum):
     RUST = 'rust'
 
 
-
 def remove_suffix(text, suffix):
     if text.endswith(suffix):
         return text[:-len(suffix)]
@@ -42,26 +41,27 @@ async def generate_rust_models(yaml_file, parser):
         raise ValueError(f"Unknown parser type: {parser}")
 
 
-async def process_yaml_files(yaml_directory):
-    yaml_files = [f for f in os.listdir(yaml_directory) if re.match(
-        r'.*(\_asyncapi|\_openapi)\.yml$', f)]
-
-    for yaml_file in yaml_files:
-        if yaml_file.endswith('_asyncapi.yml'):
-            parser = Parser.ASYNC_API
-            print(f"parsing async API yml: {yaml_file}")
-        elif yaml_file.endswith('_openapi.yml'):
-            parser = Parser.OPEN_API
-            print(f"parsing open API yml: {yaml_file}")
-        else:
-            continue
-
-        await generate_rust_models(os.path.join(yaml_directory, yaml_file), parser)
+def list_yaml(yaml_directory):
+    [f for f in os.listdir(yaml_directory) if re.match(
+        r'.yaml$', f)]
 
 
 async def main():
-    yaml_directory = './'
-    await process_yaml_files(yaml_directory)
+    # async api
+    parser = Parser.ASYNC_API
+    asyncapi_yaml_directory = './asyncapi/'
+    yaml_list = list_yaml(asyncapi_yaml_directory)
+    for yaml in yaml_list:
+        print(f"generating {yaml}")
+        await generate_rust_models(os.path.join(asyncapi_yaml_directory, yaml), parser)
+
+    # open api
+    parser = Parser.OPEN_API
+    openapi_yaml_directory = './openapi/'
+    yaml_list = list_yaml(openapi_yaml_directory)
+    for yaml in yaml_list:
+        print(f"generating {yaml}")
+        await generate_rust_models(os.path.join(openapi_yaml_directory, yaml), parser)
 
 if __name__ == "__main__":
     asyncio.run(main())
---
+++ src/main.rs
@@ -0,0 +1,13 @@
+pub enum Parser {
+    AsyncApi,
+    OpenApi,
+}
+
+pub enum Language {
+    Rust,
+    Python,
+}
+
+async fn main() {
+    println!("Hello, world!");
+}
